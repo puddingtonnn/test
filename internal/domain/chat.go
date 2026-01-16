4fe1e1cd8f4e0913a3f4f342e7feff6b0238ff18
puddingtonnn <puddingtonnn@gmail.com>
2026-01-16 23:34:02 +0900

    core

---
+++ internal/domain/chat.go
@@ -0,0 +1,18 @@
+package domain
+
+import "time"
+
+type Chat struct {
+	ID       int       `gorm:"primaryKey;autoIncrement" json:"id"`
+	Title    string    `gorm:"not null;size:100" json:"title"`
+	CreateAt time.Time `json:"create_at"`
+
+	Messages []Message `gorm:"constraint:OnDelete:CASCADE;" json:"messages,omitempty"`
+}
+
+type Message struct {
+	ID       int       `gorm:"primaryKey;autoIncrement" json:"id"`
+	ChatID   int       `gorm:"not null;index" json:"chat_id"`
+	Text     string    `gorm:"not null;size:10000" json:"text"`
+	CreateAt time.Time `json:"create_at"`
+}
